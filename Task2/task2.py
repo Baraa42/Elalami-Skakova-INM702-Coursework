e86d0826ebdad4c969af907593eee4cea156f17c
Tamila Skakova <36418704+TamilaS@users.noreply.github.com>
2021-11-24 14:21:56 +0000

    Multicollinearity

--- Task2/task2.py
+++ Task2/task2.py
@@ -212,5 +212,24 @@ plt.legend()
 plt.title('Scatter plot of distance of the points from the regression line')
 plt.show();
 
+"""
+Multicollinearity in the Linear Regression Model
+"""
 
-    
+#Importing packages
+import os
+# import numpy as np
+import pandas as pd
+import seaborn as sns
+import statsmodels.api as sm
+# import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+# from sklearn.linear_model import LinearRegression
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+#opening the dataset to work with "auto-mpg"
+path = '.'
+dataset = os.path.join("auto-mpg.csv")
+df = pd.read_csv(dataset,na_values=['NA','?'])
+# df.head(10)

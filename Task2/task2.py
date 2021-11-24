3c63a1ac60f848826b3281b871d4afdcd633b924
Tamila Skakova <36418704+TamilaS@users.noreply.github.com>
2021-11-24 14:29:33 +0000

    Finding the multicollinearity
    By exploring the dataset further, using different statistical, visualisation and VIF we determine the multicollinearity in the dataset

--- Task2/task2.py
+++ Task2/task2.py
@@ -250,3 +250,96 @@ df['horsepower'] = df['horsepower'].fillna(med)
 
 #Checking for missing values the column "Horsepower" after filling it with medians value of the column
 df["horsepower"].isnull().values.any()
+
+"""
+Exploring correlation between columns of our dataset using different methods and possible multicollinearity.
+"""
+%matplotlib inline
+# %matplotlib widget
+X1 = df["weight"].values.reshape(-1,1)
+X2 = df["mpg"].values.reshape(-1,1)
+# X3 = df["displacement"].values.reshape(-1,1)
+y = df["horsepower"].values.reshape(-1,1)
+fig = plt.figure()
+ax = plt.axes(projection = "3d")
+ax.scatter3D(X1, y, X2, "gray")
+plt.show()
+
+# ploting a plane
+
+X1 = df["weight"].values.reshape(-1, 1)
+X2 = df["mpg"].values.reshape(-1, 1)
+# X3 = df["displacement"].values.reshape(-1,1)
+y = df["horsepower"].values.reshape(-1, 1)
+
+
+def plot_plane_with_points(x, y, z):
+    X = np.hstack((x, y))
+    X = np.hstack((np.ones((x.shape[0], 1)), X))
+    theta = np.dot(np.dot(np.linalg.pinv(np.dot(X.transpose(), X)), X.transpose()), z)
+    k = int(max(np.max(x), np.max(y), np.max(z)))  # size of the plane
+
+    p1, p2 = np.mgrid[:k, :k]
+    P = np.hstack((np.reshape(p1, (k * k, 1)), np.reshape(p2, (k * k, 1))))
+    P = np.hstack((np.ones((k * k, 1)), P))
+
+    plane = np.reshape(np.dot(P, theta), (k, k));
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    ax.plot(x[:, 0], y[:, 0], z[:, 0], 'ro')  # scatter plot
+    ax.plot_surface(p1, p2, plane)  # plane plot
+
+    ax.set_xlabel('x1 label')
+    ax.set_ylabel('x2 label')
+    ax.set_zlabel('y label')
+
+    return plt.show()
+plot_plane_with_points(X1, y, X2)
+"""
+We can see that there is a high correlation between these three columns.
+Method 2. Let's have a look at 2 specific columns of the dataset and their correlation.
+"""
+import matplotlib.pyplot as plt
+X = df[["weight"]]
+y = df["horsepower"]
+plt.scatter(X, y)
+plt.plot(X, slr.predict(X), color='red', linewidth=2);
+
+"""
+as x varibale increases, y outcome variable increases in a almost perfectly correlated manner.
+
+We can also have a look at correlation between all comulmns at our datset
+"""
+#scatterplot
+sns.set()
+cols = df.columns.drop(["name"])
+sns.pairplot(df[cols], size = 2.5)
+plt.show();
+
+# heatmap
+columns = df.columns
+hm = sns.heatmap(df[columns].corr(), cbar=True, annot=True)
+
+#Using statistical analysis to find whether there is a high correlation that can impact our output
+X = df.drop(columns=["name"])
+y = df["horsepower"]
+X_with_constant = sm.add_constant(X)
+model = sm.OLS(y, X_with_constant)
+results = model.fit()
+print(results.summary())
+
+'''
+[2] The condition number is large, 8.69e+04. This might indicate that there are strong multicollinearity or other numerical problems.
+'''
+
+'''
+Also, correlation can be checked using VIF. Presence of High Variance Inflation Factor in the dataset is usually undesirable, since it highlights unreliability of your computations in regression analysis.
+'''
+X_variables = df.drop(columns=["name"])
+# [["horsepower","weight","displacement", "mpg"]]
+vif_data = pd.DataFrame()
+vif_data["feature"] = X_variables.columns
+vif_data["VIF"] = [variance_inflation_factor(X_variables.values, i) for i in range(len(X_variables.columns))]
+print(vif_data)

04bd8c025a72da4477d0e526e708736571d5a513
Tamila Skakova <36418704+TamilaS@users.noreply.github.com>
2021-11-25 15:53:01 +0000

    preparation of the data

--- Task2/multicollinearity.py
+++ Task2/multicollinearity.py
@@ -20,3 +20,23 @@ path = '.'
 dataset = os.path.join("auto-mpg.csv")
 df = pd.read_csv(dataset,na_values=['NA','?'])
 #df.head(10)
+
+
+"""
+Preparation of the dataset
+"""
+
+#checking for missing values
+df.isnull().values.any()
+
+#Checking number of NANs for each column, in order to understand how many missing values there are in a dataframe.
+print("# of NaN in each columns:", df.isnull().sum(), sep='\n')
+
+#let's fill missing values of "Horsepower" with its median of the column
+med = df['horsepower'].median()
+df['horsepower'] = df['horsepower'].fillna(med)
+
+#Checking for missing values the column "Horsepower" after filling it with medians value of the column
+df["horsepower"].isnull().values.any()
+
+
